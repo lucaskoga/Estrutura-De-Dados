3e42bfcd47c603d7e0813f818eb81c9a7465589f
lucaskoga <54293587+lucaskoga@users.noreply.github.com>
2021-06-11 18:24:13 -0300

    Update Principal.java

--- Lista 11-06/Atividades 1/Principal.java
+++ Lista 11-06/Atividades 1/Principal.java
@@ -5,18 +5,18 @@ public class Principal {
         int intval[] = new int[10];
 
         Scanner teclado = new Scanner(System.in);
-
+        //Ler a entrada
         for (int i = 0; i < 10; i++){
             System.out.println("Insira os Numeros");
             intval[i] = teclado.nextInt();
         }
-
+        //Percorre o vetor e coloca as entrada nos seu vetores e imprime
         for (int i = 0; i < intval.length; i++){
             System.out.println(intval[i]);
         }
 
         System.out.println("\nSegundo Vetor \n");
-
+        //Pega os valores do primeiro vetor e adiciona no segundo vetor e imprime
         int intval2[] = intval;
         for (int o = 0; o < intval.length; o++){
             System.out.println(intval2[o]);
