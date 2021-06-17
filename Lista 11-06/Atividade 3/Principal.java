b0efdf85a9afedfa4477a18b1ad3a579f9440514
lucaskoga <54293587+lucaskoga@users.noreply.github.com>
2021-06-17 19:30:05 -0300

    Create Principal.java

---
+++ Lista 11-06/Atividade 3/Principal.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+public class Principal {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        CarroF1 carroF1[] = new CarroF1[10];
+        int cadastros = 0;
+
+        for (int i = 0; i < carroF1.length; i++) {
+            carroF1[i] = new CarroF1();
+        }
+        if (cadastros < 10) {
+            carroF1[cadastros].cadastra("Mercedes", "Mercedes 1.6 V6", "Pirelli", "Prata", "Hamilton", "Bottas", 650);
+            cadastros++;
+        } else {
+            System.out.println("Cadastro Exedeu o Limite");
+        }
+        if (cadastros < 10) {
+            carroF1[cadastros].cadastra("Aston Martin", "Mercedes 1.6 v6", "Pirelli", "Verde", "Vettel", "Stroll", 655);
+            cadastros++;
+        } else {
+            System.out.println("Cadastro Exedeu o Limite");
+        }
+        if (cadastros < 10) {
+            carroF1[cadastros].cadastra("McLaren", "Mercedes 1.6 v6", "Pirelli", "Amarelo", "Ricciardo", "Norris", 650);
+            cadastros++;
+        } else {
+            System.out.println("Cadastro Exedeu o Limite");
+        }
+        if (cadastros < 10) {
+            carroF1[cadastros].cadastra("Alpine", "Renault 1.6 v6", "Pirelli", "Preto", "Alonso", "Ocon", 655);
+            cadastros++;
+        } else {
+            System.out.println("Cadastro Exedeu o Limite");
+        }
+        if (cadastros < 10) {
+            carroF1[cadastros].cadastra("Hass", "Ferrari 1.6 v6", "Pirelli", "Branco", "Schummacher", "Mazepin", 660);
+            cadastros++;
+        } else {
+            System.out.println("Cadastro Exedeu o Limite");
+        }
+        if (cadastros < 10) {
+            carroF1[cadastros].cadastra("Alpha Romeo", "Ferrari 1.6 v6", "Pirelli", "Vermelho", "Raikkonem", "Giovinnazzi", 640);
+            cadastros++;
+        } else {
+            System.out.println("Cadastro Exedeu o Limite");
+        }
+        if (cadastros < 10) {
+            carroF1[cadastros].cadastra("Alpha Tauri", "Honda 1.6 v6", "Pirelli", "Preto Branco", "Tsunoda", "Gasly", 640);
+            cadastros++;
+        } else {
+            System.out.println("Cadastro Exedeu o Limite");
+        }
+        if (cadastros < 10) {
+            carroF1[cadastros].cadastra("Williams", "Honda 1.6 v6", "Pirelli", "Azul Vermelhor", "Russel", "Latifi", 640);
+            cadastros++;
+        } else {
+            System.out.println("Cadastro Exedeu o Limite");
+        }
+        if (cadastros < 10) {
+            carroF1[cadastros].cadastra("Red Bull", "Honda 1.6 v6", "Pirelli", "Preto Vermelho", "Verstappen", "Perez", 630);
+            cadastros++;
+        } else {
+            System.out.println("Cadastro Exedeu o Limite");
+        }
+        if (cadastros < 10) {
+            carroF1[cadastros].cadastra("Ferrari", "Ferrari 1.6 v6", "Pirelli", "Vermelho", "Sainz", "Leclerc", 630);
+            cadastros++;
+        } else {
+            System.out.println("Cadastro Exedeu o Limite");
+        }
+
+        System.out.println("Monstras todo os carro cadastrados");
+        for (int i = 0; i < cadastros; i++) {
+            carroF1[i].imprimir();
+        }
+        String buscaEquipe;
+        leitor.nextLine();
+        System.out.print("Nome da equipe para pesquisa:");
+        buscaEquipe = leitor.nextLine();
+        for (int i = 0; i < cadastros; i++) {
+            if (buscaEquipe.equalsIgnoreCase(carroF1[i].equipe)) {
+                System.out.println("=-=-Dados do Carro Pesquisado-=-=");
+                carroF1[i].imprimir();
+            }
+        }
+    }
+}
