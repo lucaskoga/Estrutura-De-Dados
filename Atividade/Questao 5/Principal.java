a821b6f462ae3302e1f9b9e6d1dbd5f037db6373
lucaskoga <54293587+lucaskoga@users.noreply.github.com>
2021-08-08 18:41:54 -0300

    Update Principal.java

--- Atividade/Questao 5/Principal.java
+++ Atividade/Questao 5/Principal.java
@@ -2,12 +2,13 @@ public class Principal {
 
 
     public static void main(String[] args) {
-
+        // Foi Criado os vetores G e R de 13 possicoes;
         int intVetG[] = new int[13];
         int intVetR[] = new int[13];
+        // E também um ponteiro para ajudar a fazer a contagem de quantos acerto o apostador teve;
         int pontNumeroAcerto = -1;
 
-
+        //  Inserindo todos os valores no vetor G
         intVetG[0] = 1;
         intVetG[1] = 2;
         intVetG[2] = 3;
@@ -22,7 +23,7 @@ public class Principal {
         intVetG[11] = 12;
         intVetG[12] = 13;
 
-
+        // Inserindo todos os valores no vetor R
         intVetR[0] = 1;
         intVetR[1] = 2;
         intVetR[2] = 3;
@@ -37,10 +38,12 @@ public class Principal {
         intVetR[11] = 12;
         intVetR[12] = 13;
 
+        // Um for para fazer a imprecão do vetor R
         for (int i = 0; i < intVetR.length; i++){
             System.out.println(intVetR[i]);
         }
-
+        /* Aqui e o responsavel por percorre todo o vetor e ver se um vetor e igual ao outro, e também e responsavel
+        por ver o total de numero igual ao vetor ganhador. */
         if (pontNumeroAcerto == -1){
             pontNumeroAcerto = 0;
             for (int i = 0; i < 13; i++){
