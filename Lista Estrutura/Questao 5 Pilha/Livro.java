4caccf3640bc880cbd1529cbe5108b43fffcaaa3
Lucas Koga <54293587+lucaskoga@users.noreply.github.com>
2021-09-12 19:33:08 -0300

    Create Livro.java

---
+++ Lista Estrutura/Questao 5 Pilha/Livro.java
@@ -0,0 +1,16 @@
+public class Livro {
+
+    String nomeLivro;
+    String autor;
+
+    public Livro(String n, String a) {
+        nomeLivro = n;
+        autor = a;
+
+    }
+
+    public String toString() {
+        return "Livro: " + nomeLivro + " autor: " + autor;
+    }
+
+}
