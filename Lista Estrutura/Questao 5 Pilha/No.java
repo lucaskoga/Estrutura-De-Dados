3d972ffffb1a882872c882b3fb0f471b479f710c
Lucas Koga <54293587+lucaskoga@users.noreply.github.com>
2021-09-12 19:32:52 -0300

    Create No.java

---
+++ Lista Estrutura/Questao 5 Pilha/No.java
@@ -0,0 +1,35 @@
+public class No {
+
+    Object obj;
+    No proximo;
+    No anterior;
+
+    public No(Object o) {
+        obj = o;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public String toString() {
+        return obj.toString();
+    }
+
+    public void setProximo(No n) {
+        proximo = n;
+    }
+
+    public No getProximo() {
+        return proximo;
+    }
+
+    public void setAnterior(No n) {
+        anterior = n;
+    }
+
+    public No getAnterior() {
+        return anterior;
+    }
+
+}
